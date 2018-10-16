c518fc0d0d19a5c64580c47d38c5fde7f3159f31
zjcasas <zjcasas@gmail.com>
2018-10-16 00:02:48 -0400

    Making the Feats page use AJAX
    removing ajax code from spells and using it for feats. I think it'll be easier to do with the feats. Although after working through it, I could've probably made it work fine with spells. There are just fewer feats.

---
+++ dungeons_and_dragons_tools/scripts/feats.js
@@ -0,0 +1,96 @@
+$(window).ready(function(){
+    //attempting to use ajax to pull info from csv
+    //it worked, but it isn't perfect so I'll have to come back to it
+    var feats;
+    var feats2 = [];
+    var feats3 = [];
+    $.ajax({
+        url: 'feats.csv',
+        dataType: 'text',
+        success: function(data) {
+            feats = data.split(/\r?\n|\r/);
+            console.log("success");
+            parseData();
+            displayData();
+        },
+        fail: function(error) {
+            console.log("error");
+            console.log(error);
+        }
+    })
+
+    function parseData() {
+        for (var i = 0; i < feats.length; i++) {
+            feats2.push(feats[i].split(","));
+        }
+        for (var i = 1; i < (feats2.length - 1); i++) {
+            for (var j = 0; j <feats2[i].length; j++){
+                if (j === 0) {
+                    feats3[(i-1)] = '{ "' + feats2[0][0] + '":';
+                    feats3[(i-1)] = feats3[(i-1)] + '"' + feats2[i][0] + '", ';
+                }
+                else if (j === (feats2[i].length -1)) {
+                    feats3[(i-1)] = feats3[(i-1)] + '"' + feats2[0][j] + '":';
+                    feats3[(i-1)] = feats3[(i-1)] + '"' + feats2[i][j] + '"}';
+                }
+                else {
+                    feats3[(i-1)] = feats3[(i-1)] + '"' + feats2[0][j] + '":';
+                    feats3[(i-1)] = feats3[(i-1)] + '"' + feats2[i][j] + '", ';
+                }
+            }
+        }
+        for (var i = 0; i < feats3.length; i++) {
+            feats3[i] = JSON.parse(feats3[i]);
+        }
+    }
+
+    function displayData() {
+        for (var i = 0; i < feats3.length; i++) {
+            var html = `<div>
+                            <h4>
+                                <span class="turn">
+                                    <i class="fas fa-angle-down"></i>
+                                </span>
+                                ${feats3[i].name}
+                            </h4>
+                            <section>
+                            <p class="prereq">
+                                Prerequisite ${feats3[i].prereq}
+                            </p>
+                                <p>
+                                    ${feats3[i].p1}
+                                    <ul>
+            							<li>
+                                        ${feats3[i].bullet1}
+                                        </li>
+            							<li>
+                                        ${feats3[i].bullet2}
+                                        </li>
+            							<li>
+                                        ${feats3[i].bullet3}
+                                        </li>
+                                        <li>
+                                        ${feats3[i].bullet4}
+                                        </li>
+            						</ul>
+                                </p>
+                                <p>
+                                    ${feats3[i].p2}
+                                </p>
+                                <p>
+                                    ${feats3[i].p3}
+                                </p>
+                                <p>
+                                    ${feats3[i].p4}
+                                </p>
+                                <p>
+                                    ${feats3[i].p5}
+                                </p>
+                            </section>
+                        </div>`
+            $('.feats').append(html);
+        }
+        $('li:not(:contains("."))').hide();
+        $('.prereq:not(:contains(\u2014))').hide();
+    }
+});
